fb6ce50af7ec9038761bb786d60ffd2bb3bb3c00
Pavel Krasnov <pavel.krasnov.work@gmail.com>
2020-04-08 19:25:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import Card from './Card';
+import { pickCardPairsWithRandomColor } from './utils';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CARDS_NUMBER = 16;
 
-export default App;
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: pickCardPairsWithRandomColor(CARDS_NUMBER),
+            openCard: null,
+            canClick: true
+        };
+    }
+
+    onCardClick = (event) => {
+        const {cards, openCard} = this.state;
+        const id = Number(event.target.id.split('-')[1]);
+        const newOpenCard = {...cards[id], open: true};
+        let newCards = cards.map((item, index) => index === id ? newOpenCard : item);
+        this.setState({...this.state, cards: newCards, canClick: false}, () => {
+            if (openCard) { 
+                if (openCard.color === newOpenCard.color) {
+                    console.log('Color match!');
+                    this.setState({...this.state, openCard: null, canClick: true});
+                } else {
+                    setTimeout(() => {
+                        console.log('No match!');
+                        newCards = cards.map((item, index) => index === id || index === openCard.id ? {...item, open: false} : item);
+                        this.setState({...this.state, cards: newCards, openCard: null, canClick: true});
+                    }, 1000);
+                }
+            } else {
+                console.log('New open card!');
+                this.setState({...this.state, openCard: newOpenCard, canClick: true});
+            }
+        });
+    };
+
+    onNewGameBtnClick = (event) => {
+        event.preventDefault();
+        this.setState({cards: pickCardPairsWithRandomColor(CARDS_NUMBER), openCard: null});
+    };
+
+    render() {
+        const cardElements = this.state.cards.map((item, index) =>
+            <Card
+                key={index}
+                id={item.id}
+                color={item.color}
+                open={item.open}
+                onClick={this.state.canClick ? this.onCardClick : null} 
+            />);
+        return (
+            <div id="app-container">
+                <button id="new-game-btn" onClick={this.onNewGameBtnClick}>New game</button>
+                <div id="cards-container">
+                    {cardElements}
+                </div>
+            </div>
+        );
+    }
+};
