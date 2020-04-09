cb3e7cdafb656242b52fd3a68b5fa231fa49ac6c
pavel-krasnov <pavel.krasnov.work@gmail.com>
2020-04-09 06:42:04 +0100

    Updates

---
+++ precache-manifest.257caaea3bcc0df8ed18f7a7c2022b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034bf41c2ffb2e218f3addc94bec8b9e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "44b38b578c58d3fdc873",
+    "url": "/memory-game/static/css/main.9ef14d7d.chunk.css"
+  },
+  {
+    "revision": "fd24c308814fdf024a32",
+    "url": "/memory-game/static/js/2.416322bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.416322bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b38b578c58d3fdc873",
+    "url": "/memory-game/static/js/main.f822797f.chunk.js"
+  },
+  {
+    "revision": "37eb60d43a600f236727",
+    "url": "/memory-game/static/js/runtime-main.38e44c2e.js"
+  }
+]);
